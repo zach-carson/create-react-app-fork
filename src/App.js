40391b905623556acf0251e5d9bc084043110a7b
Jps709 <jps7095@psu.edu>
2023-02-05 19:24:57 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import './App.css';
 
 function App() {
   return (
+
+<>
     <div class="outline">
       <div class="lecard">
-      <h2>Some Nerd</h2>
-      <img src="https://i.kym-cdn.com/news_feeds/icons/mobile/000/037/359/05b.jpg" alt="Obi-Wan headshot" />
-      <p class="details">Hello there <br></br> Lightsaber go <i>brrrr</i></p>     
-       </div>
+        <h2>Some Nerd</h2>
+        <img src="https://i.kym-cdn.com/news_feeds/icons/mobile/000/037/359/05b.jpg" alt="Obi-Wan headshot" />
+        <p class="details">Hello there <br></br> Lightsaber go <i>brrrr</i></p>
+      </div>
     </div>
+  <div class="buttons">
+  
+  <button class="copyCat">Copy</button>
+  <button id="del">Delete</button>
+  <button class="btnDet">Toggle Details</button> 
+  <button class="btnCT">Change Title</button>
+  <button  class="btnBG">Change Background Color</button>
+  
+  </div>
+  
+</>
   );
 }
 
