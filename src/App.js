ac94f7d874ed097399aa2d0f318327d12b2c7a32
zach-carson <122495829+zach-carson@users.noreply.github.com>
2023-02-12 13:27:54 -0500

    fork

--- src/App.js
+++ src/App.js
@@ -5,23 +5,31 @@ function App() {
   return (
 
 <>
-    <div class="outline">
-      <div class="lecard">
-        <h2>Some Nerd</h2>
-        <img src="https://i.kym-cdn.com/news_feeds/icons/mobile/000/037/359/05b.jpg" alt="Obi-Wan headshot" />
-        <p class="details">Hello there <br></br> Lightsaber go <i>brrrr</i></p>
-      </div>
-    </div>
-  <div class="buttons">
-  
-  <button class="copyCat">Copy</button>
-  <button id="del">Delete</button>
-  <button class="btnDet">Toggle Details</button> 
-  <button class="btnCT">Change Title</button>
-  <button  class="btnBG">Change Background Color</button>
-  
+
+  <div class="card" id="card1">
+  <div>
+  <div class="title-header">
+    <h2> Jalen Hurts </h2>
   </div>
+  <img class = "img" src="https://th.bing.com/th/id/OIP.NX-MOMDx5uAkx4YnlPWexAHaE7?pid=ImgDet&rs=1" alt="jalen hurts"></img>
+  <p class = "desc"> Jalen Hurts is a football player </p>
+  <a href="https://hax.psu.edu/">
+  <button class="btn" id='details'> Details </button>
+  </a>
+</div>
+</div>
+
+<button class="duplicate"> Duplicate card </button>
+
+<button class="newcolor"> Toggle background color </button>
   
+<button class="something"> Something else </button>
+  
+<button class="delete"> Delete last card </button>
+
+  
+<div id="destination"></div>
+
 </>
   );
 }
