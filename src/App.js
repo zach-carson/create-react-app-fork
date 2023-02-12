7cededa39f271e8842861a208c8b8dd0cd420e98
zach-carson <122495829+zach-carson@users.noreply.github.com>
2023-02-12 13:13:56 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,25 +4,29 @@ import './App.css';
 function App() {
   return (
 
-<div>
-    <div class="outline">
-      <div class="lecard">
-        <h2>Some Nerd</h2>
-        <img src="https://i.kym-cdn.com/news_feeds/icons/mobile/000/037/359/05b.jpg" alt="Obi-Wan headshot" />
-        <p class="details">Hello there <br></br> Lightsaber go <i>brrrr</i></p>
-      </div>
-    </div>
-  <div class="buttons">
-  
-  <button class="copyCat">Copy</button>
-  <button id="del">Delete</button>
-  <button class="btnDet">Toggle Details</button> 
-  <button class="btnCT">Change Title</button>
-  <button  class="btnBG">Change Background Color</button>
-  
+<div class="card" id="card1">
+  <div>
+  <div class="title-header">
+    <h2> Jalen Hurts </h2>
   </div>
-  
+  <img class = "img" src="https://th.bing.com/th/id/OIP.NX-MOMDx5uAkx4YnlPWexAHaE7?pid=ImgDet&rs=1" alt="jalen hurts"></img>
+  <p class = "desc"> Jalen Hurts is a football player </p>
+  <a href="https://hax.psu.edu/">
+  <button class="btn" id='details'> Details </button>
+  </a>
 </div>
+</div>
+
+<button class="duplicate"> Duplicate card </button>
+
+<button class="newcolor"> Toggle background color </button>
+  
+<button class="something"> Something else </button>
+  
+<button class="delete"> Delete last card </button>
+
+  
+<div id="destination"></div>
   );
 }
 document.querySelector('.copyCat').addEventListener('click', function(e) {
